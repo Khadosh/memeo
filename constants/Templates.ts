08be0c564966722e31d2f045bbbcce53551964c1
Joaquin Nader <joaquin@getstreamlane.com>
2026-03-12 09:17:26 -0300

    feat(templates): add top 10 classic meme templates

--- constants/Templates.ts
+++ constants/Templates.ts
@@ -1,15 +1,15 @@
 export const TEMPLATES = [
   {
+    id: 'doge',
+    name: 'Doge',
+    image: require('@/assets/memes/doge.jpg'),
+    publicUrl: 'https://i.imgflip.com/4t0m5.jpg'
+  },
+  {
     id: 'distracted-boyfriend',
     name: 'Distracted Boyfriend',
     image: require('@/assets/memes/distracted-boyfriend.jpg'),
     publicUrl: 'https://i.imgflip.com/1ur9b0.jpg'
-  },
-  {
-    id: 'drake-hotline-bling',
-    name: 'Drake Hotline Bling',
-    image: require('@/assets/memes/drake-hotline-bling.jpg'),
-    publicUrl: 'https://i.imgflip.com/30b1gx.jpg'
   },
   {
     id: 'disaster-girl',
@@ -18,21 +18,51 @@ export const TEMPLATES = [
     publicUrl: 'https://i.imgflip.com/23ls.jpg'
   },
   {
-    id: 'two-buttons',
-    name: 'Two Buttons',
-    image: require('@/assets/memes/two-buttons.jpg'),
-    publicUrl: 'https://i.imgflip.com/1g8my4.jpg'
-  },
-  {
-    id: 'change-my-mind',
-    name: 'Change My Mind',
-    image: require('@/assets/memes/change-my-mind.jpg'),
-    publicUrl: 'https://i.imgflip.com/24y43o.jpg'
+    id: 'success-kid',
+    name: 'Success Kid',
+    image: require('@/assets/memes/success-kid.jpg'),
+    publicUrl: 'https://i.imgflip.com/1bhk.jpg'
   },
   {
     id: 'woman-yelling-cat',
-    name: 'Woman Yelling at Cat',
+    name: 'Woman Yelling at a Cat',
     image: require('@/assets/memes/woman-yelling-cat.jpg'),
     publicUrl: 'https://i.imgflip.com/345v97.jpg'
   },
+  {
+    id: 'harold',
+    name: 'Hide the Pain Harold',
+    image: require('@/assets/memes/harold.jpg'),
+    publicUrl: 'https://i.imgflip.com/1g8my4.jpg'
+  },
+  {
+    id: 'drake-hotline-bling',
+    name: 'Drake (Hotline Bling)',
+    image: require('@/assets/memes/drake-hotline-bling.jpg'),
+    publicUrl: 'https://i.imgflip.com/30b1gx.jpg'
+  },
+  {
+    id: 'spiderman',
+    name: 'Spider-Man Pointing',
+    image: require('@/assets/memes/spiderman.jpg'),
+    publicUrl: 'https://i.imgflip.com/1tkjq9.jpg'
+  },
+  {
+    id: 'this-is-fine',
+    name: 'This is Fine',
+    image: require('@/assets/memes/this-is-fine.jpg'),
+    publicUrl: 'https://i.imgflip.com/1x6f24.jpg'
+  },
+  {
+    id: 'ancient-aliens',
+    name: 'Ancient Aliens',
+    image: require('@/assets/memes/ancient-aliens.jpg'),
+    publicUrl: 'https://i.imgflip.com/26am.jpg'
+  },
+  {
+    id: 'tom-cruise',
+    name: 'Tom Cruise Laughing',
+    image: require('@/assets/memes/tom-cruise.jpeg'),
+    publicUrl: 'https://i.imgflip.com/13s2v.jpg' // Real 'Tom Cruise Laughing' meme URL
+  }
 ];
